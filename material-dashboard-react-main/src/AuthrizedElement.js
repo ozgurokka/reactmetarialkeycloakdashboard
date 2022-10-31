0c1b9b15f6a6dca0399f6679f86639979bc51c9a
ozgur <ozgur>
2022-10-31 14:26:52 +0300

    first

---
+++ material-dashboard-react-main/src/AuthrizedElement.js
@@ -0,0 +1,21 @@
+import { useKeycloak } from '@react-keycloak/web';
+import { Routes } from 'react-router-dom';
+
+
+
+export default function AuthrizedElement({ roles, children }){
+    const keycloak = useKeycloak().keycloak;
+
+    const isAutherized = () => {
+        if (keycloak && roles) {
+            return roles.some(r => {
+                const realm =  keycloak.hasRealmRole(r);
+                const resource = keycloak.hasResourceRole(r);
+                return realm || resource;
+            });
+        }
+        return false;
+    }
+
+    return isAutherized() && children
+}
---
+++ material-dashboard-react-main/src/Keycloak.js
@@ -0,0 +1,10 @@
+import Keycloak from "keycloak-js";
+
+const keycloak = new Keycloak({
+ url: "http://localhost:8080",
+ realm: "demo",
+ clientId: "react-app",
+ promiseType :"native"
+});
+
+export default keycloak;
